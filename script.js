e870f8c6c465c615f57b76a2e9b21206e6c35b2c
mannmanjeet988 <120735535+mannmanjeet988@users.noreply.github.com>
2023-07-17 18:10:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,9 @@
 //complete this code
-class Animal {
-    constructor(species){
-        this.species=species;
-    }
-   
-    get speciesName(){
-        return this.species;
-    }
+class Animal {}
 
-    makeSound(){
-        console.log(`${this.species} makes sound`)
-    }
-}
+class Dog extends Animal {}
 
-class Dog extends Animal {
-    constructor(){
-        super("Bulldog")
-    }
-    bark(){
-        console.log("woof")  
-    }
-}
-
-class Cat extends Animal {
-    constructor(){
-        super("Persian cat")
-    }
-    purr(){
-        console.log("purr")  
-    }
-}
-
-var animal = new Animal("Asian elephant")
-animal.makeSound();
-
-var cat = new Cat()
-cat.purr();
-
-var dog = new Dog()
-dog.bark();
-
-
+class Cat extends Animal {}
 
 // Do not change the code below this line
 window.Animal = Animal;
